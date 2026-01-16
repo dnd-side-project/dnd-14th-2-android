75e3316ecc7862948f71fa2a0dfa6ab759c5994b
김의현 <hyunkim6790@gmail.com>
2026-01-16 16:42:22 +0900

    feat(auth): 구글 및 카카오 로그인 기능 구현
    - Credential Manager를 사용하여 Google 로그인 로직 구현
    - 카카오 SDK를 사용하여 Kakao 로그인 로직 구현
    - 각 소셜 로그인 결과를 처리하는 Manager 클래스 추가

---
+++ presentation/src/main/java/com/smtm/pickle/presentation/common/auth/KakaoLoginManager.kt
@@ -0,0 +1,40 @@
+package com.smtm.pickle.presentation.common.auth
+
+import android.content.Context
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.user.UserApiClient
+import com.smtm.pickle.presentation.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class KakaoLoginManager @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+
+    fun login(
+        onSuccess: (String) -> Unit,
+        onFailure: (Throwable) -> Unit
+    ) {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            when {
+                error != null -> onFailure(error)
+                token != null -> onSuccess(token.accessToken)
+                else -> onFailure(Exception(context.getString(R.string.unknown_error)))
+            }
+        }
+
+        // 카카오톡 로그인 가능 여부에 따라 처리
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+            UserApiClient.instance.loginWithKakaoTalk(context, callback = callback)
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+        }
+    }
+
+}
+
+fun logout(onCompleted: () -> Unit) {
+    UserApiClient.instance.logout { error ->
+        onCompleted()
+    }
+}
