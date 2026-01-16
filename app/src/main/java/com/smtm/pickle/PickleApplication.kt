c70331efbd9d6abf60ca377228ed9b39abcf4d94
김의현 <hyunkim6790@gmail.com>
2026-01-16 14:43:15 +0900

    feat: 카카오 SDK 및 Timber 초기화 설정
    - Application 클래스에서 Kakao SDK 초기화 코드 추가
    - 디버그 빌드에서 Timber 로깅 라이브러리 활성화

--- app/src/main/java/com/smtm/pickle/PickleApplication.kt
+++ app/src/main/java/com/smtm/pickle/PickleApplication.kt
@@ -2,6 +2,17 @@ package com.smtm.pickle
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import com.kakao.sdk.common.KakaoSdk
+import timber.log.Timber
 
 @HiltAndroidApp
-class PickleApplication : Application()
+class PickleApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // TODO: 카카오 API 키 사용 ("" 지우기)
+        KakaoSdk.init(this, "BuildConfig.KAKAO_NATIVE_APP_KEY")
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
