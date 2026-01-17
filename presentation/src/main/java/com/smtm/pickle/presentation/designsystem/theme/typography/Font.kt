15f1ddf5f8c41dab32775c309c812020e07e0fea
김의현 <hyunkim6790@gmail.com>
2026-01-17 20:24:36 +0900

    feat(designsystem): Pretendard 폰트 및 타이포그래피 시스템 정의
    - Pretendard-Variable 폰트 파일 추가
    - 폰트 Weight별 FontFamily 정의 (Pretendard)
    - 디자인 시스템 타이포그래피(PickleTypography) 클래스 및 스타일 정의
    - 기존 Material Typography 삭제

---
+++ presentation/src/main/java/com/smtm/pickle/presentation/designsystem/theme/typography/Font.kt
@@ -0,0 +1,26 @@
+package com.smtm.pickle.presentation.designsystem.theme.typography
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.smtm.pickle.presentation.R
+
+val Pretendard = FontFamily(
+    Font(
+        resId = R.font.PretendardVariable,
+        weight = FontWeight.Bold,
+    ),
+    Font(
+        resId = R.font.PretendardVariable,
+        weight = FontWeight.SemiBold,
+    ),
+    Font(
+        resId = R.font.PretendardVariable,
+        weight = FontWeight.Medium,
+    ),
+    Font(
+        resId = R.font.PretendardVariable,
+        weight = FontWeight.Normal,
+    ),
+
+)
