f6ca4c4d65b8ad6a4bab0e8d82b9f2dc77c21830
김의현 <hyunkim6790@gmail.com>
2026-01-17 13:53:53 +0900

    feat(navigation): 각 화면별 임시 UI 구현
    - 화면 이동을 위한 임시 버튼을 각 화면에 구현

--- presentation/src/main/java/com/smtm/pickle/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/smtm/pickle/presentation/home/HomeScreen.kt
@@ -1,7 +1,28 @@
 package com.smtm.pickle.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Home Screen")
+        Button(
+            onClick = onClick
+        ) {
+            Text(text = "Go to Mypage")
+        }
+    }
 }
