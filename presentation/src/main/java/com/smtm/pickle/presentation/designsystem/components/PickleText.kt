98dbfc6f2d5979e33246efa710562750e7236a52
김의현 <hyunkim6790@gmail.com>
2026-01-18 23:30:26 +0900

    feat(designsystem): PickleSupportingText 컴포저블 추가
    - 입력 상태(Error, Success, Idle)에 따라 색상이 변경되는 `PickleSupportingText` 컴포저블 구현

---
+++ presentation/src/main/java/com/smtm/pickle/presentation/designsystem/components/PickleText.kt
@@ -0,0 +1,24 @@
+package com.smtm.pickle.presentation.designsystem.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.smtm.pickle.presentation.designsystem.components.textfield.model.InputState
+import com.smtm.pickle.presentation.designsystem.theme.PickleTheme
+
+@Composable
+fun PickleSupportingText(
+    message: String ,
+    inputState: InputState,
+) {
+    val color = when (inputState) {
+        is InputState.Error -> PickleTheme.colors.error50
+        is InputState.Success -> PickleTheme.colors.primary400
+        InputState.Idle -> PickleTheme.colors.gray600
+    }
+
+    Text(
+        text = message,
+        style = PickleTheme.typography.caption2Regular,
+        color = color,
+    )
+}
