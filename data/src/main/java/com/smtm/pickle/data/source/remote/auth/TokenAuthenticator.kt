514e301357c336d1060a630047c0613864f1cbbb
김의현 <hyunkim6790@gmail.com>
2026-01-19 16:18:06 +0900

    feat(auth): 토큰 재발급 로직 구현
    - OkHttp Authenticator를 사용하여 액세스 토큰 만료 시 자동으로 토큰을 재발급하는 로직 추가
    - 리프레시 토큰을 사용해 새로운 액세스/리프레시 토큰을 발급받고, 실패한 요청을 재시도.
    - 리프레시 토큰이 만료되면 저장된 토큰을 모두 삭제

---
+++ data/src/main/java/com/smtm/pickle/data/source/remote/auth/TokenAuthenticator.kt
@@ -0,0 +1,70 @@
+package com.smtm.pickle.data.source.remote.auth
+
+import com.smtm.pickle.data.source.remote.api.AuthService
+import com.smtm.pickle.data.source.remote.model.RefreshRequest
+import com.smtm.pickle.domain.model.auth.AuthToken
+import com.smtm.pickle.domain.provider.TokenProvider
+import kotlinx.coroutines.runBlocking
+import okhttp3.Authenticator
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.Route
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TokenAuthenticator @Inject constructor(
+    private val tokenProvider: TokenProvider,
+    private val authService: AuthService,
+) : Authenticator {
+
+    override fun authenticate(route: Route?, response: Response): Request? {
+
+        if (responseCount(response) >= 2) return null
+
+        val refreshToken = runBlocking {
+            tokenProvider.getRefreshToken()
+        } ?: return null
+
+        // RefreshToken으로 새로운 Token 발급
+        val newToken = runBlocking {
+            runCatching {
+                authService.refreshToken(
+                    RefreshRequest(refreshToken)
+                )
+            }.getOrNull()
+        } ?: run {
+            // 리프레시 토큰 만료시 로그아웃 처리
+            runBlocking { tokenProvider.clearToken() }
+            return null
+        }
+
+        // 토큰 저장
+        runBlocking {
+            tokenProvider.saveToken(
+                AuthToken(
+                    access = newToken.accessToken,
+                    refresh = newToken.refreshToken
+                )
+            )
+        }
+
+        // 401 엑세스 토큰 만료로 실패한 요청에 새 토큰을 넣어 재시도
+        return response.request.newBuilder()
+            .header("Authorization", "Bearer ${newToken.accessToken}")
+            .build()
+    }
+
+    // 순환 참조 방지
+    private fun responseCount(response: Response): Int {
+        var count = 1
+        var prior = response.priorResponse
+
+        while (prior != null) {
+            count++
+            prior = prior.priorResponse
+        }
+
+        return count
+    }
+}
