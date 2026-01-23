fab7a12ec6c1bc2303dd1f6aa1fbb085b9478501
김의현 <hyunkim6790@gmail.com>
2026-01-23 16:17:20 +0900

    feat(designsystem): PickleCard 컴포저블 추가
    - 재사용 가능한 `PickleCard` 컴포저블을 디자인 시스템에 추가했습니다.
    - `PickleCard`는 그림자, 테두리, 색상 등을 커스텀할 수 있는 옵션을 제공합니다.
    - 개발 환경에서 `PickleCard`의 시각적 형태를 확인할 수 있도록 `@Preview`를 추가했습니다.

---
+++ presentation/src/main/java/com/smtm/pickle/presentation/designsystem/components/PickleCard.kt
@@ -0,0 +1,73 @@
+package com.smtm.pickle.presentation.designsystem.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.smtm.pickle.presentation.common.extension.pickleShadow
+import com.smtm.pickle.presentation.designsystem.theme.PickleTheme
+import com.smtm.pickle.presentation.designsystem.theme.dimension.Dimensions
+
+@Composable
+fun PickleCard(
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(16.dp),
+    color: Color = PickleTheme.colors.base0,
+    hasBoarder: Boolean = false,
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    val colors = PickleTheme.colors
+    val border = remember(hasBoarder) {
+        if (hasBoarder) {
+            BorderStroke(width = 2.dp, color = colors.primary300)
+        } else {
+            null
+        }
+    }
+
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(Dimensions.radiusSurface),
+        color = color,
+        contentColor = colors.gray700,
+        border = border,
+    ) {
+        Column(
+            modifier = Modifier.padding(contentPadding),
+            content = content,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PickleCardPreview() {
+    PickleTheme {
+        PickleCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 50.dp)
+                .pickleShadow(),
+            hasBoarder = true,
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("다음")
+            }
+        }
+    }
+}
