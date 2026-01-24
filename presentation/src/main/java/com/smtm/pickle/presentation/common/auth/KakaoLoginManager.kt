0cccdc603584b706dce791744e374ed657211a88
김의현 <hyunkim6790@gmail.com>
2026-01-24 23:30:08 +0900

    feat(auth): 카카오 권장 로그인 취소 시 예외 처리 추가
    - 카카오톡으로 로그인 시도 중 사용자가 명시적으로 로그인을 취소한 경우(`ClientErrorCause.Cancelled`), 카카오 계정으로 다시 로그인하는 대신 즉시 실패로 처리하도록 로직을 수정했습니다.
    - 사용자가 카카오톡이 설치되어 있지만 계정이 연결되지 않은 경우에는 기존과 같이 카카오 계정 로그인으로 fallback 됩니다.
    
    https://developers.kakao.com/docs/latest/ko/kakaologin/android#kakaologin-sample

--- presentation/src/main/java/com/smtm/pickle/presentation/common/auth/KakaoLoginManager.kt
+++ presentation/src/main/java/com/smtm/pickle/presentation/common/auth/KakaoLoginManager.kt
@@ -3,6 +3,8 @@ package com.smtm.pickle.presentation.common.auth
 import android.content.Context
 import androidx.compose.runtime.Stable
 import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.smtm.pickle.presentation.R
 import dagger.hilt.android.qualifiers.ActivityContext
@@ -28,7 +30,28 @@ class KakaoLoginManager @Inject constructor(
 
         // 카카오톡 로그인 가능 여부에 따라 처리
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            UserApiClient.instance.loginWithKakaoTalk(context, callback = callback)
+            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                when {
+                    token != null -> {
+                        onSuccess(token.accessToken)
+                    }
+
+                    error != null -> {
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            onFailure(error)
+                        } else {
+                            // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                        }
+                    }
+
+                    else -> {
+                        onFailure(Exception(context.getString(R.string.unknown_error)))
+                    }
+                }
+            }
         } else {
             UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
         }
