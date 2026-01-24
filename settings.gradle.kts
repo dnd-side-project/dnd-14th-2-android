708715f542f5aceb4e33854552995f4ee3d1b074
김의현 <hyunkim6790@gmail.com>
2026-01-24 23:29:51 +0900

    build(deps): 카카오 저장소 조회 라이브러리 그룹 제한
    - Gradle의 `exclusiveContent`를 사용하여, 카카오(Kakao) Maven 저장소에서는 `com.kakao.sdk`와 `com.kakao.i` 그룹의 라이브러리만 조회하도록 설정했습니다. 이를 통해 의존성 조회 성능을 최적화하고 빌드 안정성을 높였습니다.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,20 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+
+        // https://docs.gradle.org/current/userguide/filtering_repository_content.html?utm_source=chatgpt.com
+        // forRepository에 선언된 kakao 저장소는 filter에 선언된 라이브러리 그룹만 조회
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://devrepo.kakao.com/nexus/content/groups/public/")
+                }
+            }
+            filter {
+                includeGroup("com.kakao.sdk")
+                includeGroup("com.kakao.i")
+            }
+        }
     }
 }
 
